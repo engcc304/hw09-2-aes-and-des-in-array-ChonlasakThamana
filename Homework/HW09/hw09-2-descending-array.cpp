10add64be7620ac84ccc736edc13a5e5efbd6894
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-09-17 13:46:00 +0700

    9-2

--- Homework/HW09/hw09-2-descending-array.cpp
+++ Homework/HW09/hw09-2-descending-array.cpp
@@ -22,4 +22,58 @@
         0-99 : 1 1 4 5 6 9 13
         99-0 : 13 9 6 5 4 1 1
 
-*/
+*/#include <stdio.h>
+
+int main() {
+    int data[100];
+    int input;
+    int size = 0;
+
+    // รับข้อมูลจากผู้ใช้
+    while (1) {
+        printf("Input: ");
+        scanf("%d", &input);
+        if (input == -1) {
+            break;
+        }
+        data[size++] = input;
+    }
+
+    // เรียงข้อมูลจากน้อยไปมาก (จากต่ำสุดไปยังสูงสุด)
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (data[j] > data[j + 1]) {
+                int temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+
+    // แสดงผลลัพธ์เรียงข้อมูลจากน้อยไปมาก
+    printf("0-99 : ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+
+    // เรียงข้อมูลจากมากไปน้อย (จากสูงสุดไปยังต่ำสุด)
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (data[j] < data[j + 1]) {
+                int temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+
+    // แสดงผลลัพธ์เรียงข้อมูลจากมากไปน้อย
+    printf("99-0 : ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
